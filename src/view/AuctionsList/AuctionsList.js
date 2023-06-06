f148a5ead68d4cc323534b874073d2f6602a1472
PatSkyman <patrickmuthii97@gmail.com>
2023-06-06 17:31:45 -0400

    auctionlist page modification

--- src/view/AuctionsList/AuctionsList.js
+++ src/view/AuctionsList/AuctionsList.js
@@ -5,16 +5,29 @@ import List from '@mui/joy/List';
 import { Link } from 'react-router-dom';
 import { useAllAuctions } from '../../data';
 import Result from './Result';
-
 import Filter from './plp/Filter';
 import SortButton from './plp/SortButton';
 
+const AuctionListContainer = styled('div')({
+  position: 'relative',
+});
+
+const SortButtonContainer = styled('div')({
+  position: 'absolute',
+  top: 0,
+  right: 10,
+  zIndex: 100, // Adjust the z-index value as needed
+});
 
 export default function AuctionList() {
   const results = useAllAuctions();
 
   return (
     <>
+      <AuctionListContainer>
+        <SortButtonContainer>
+          <SortButton />
+        </SortButtonContainer>
         <Sheet
           variant="outlined"
           sx={{
@@ -22,25 +35,22 @@ export default function AuctionList() {
             flexDirection: 'column',
             gap: 20,
             width: 'auto',
-            height: '300',
-            margin: 1,
+            height: '300px', // Adjust the height as needed
+            margin: '40px 0 20px 0', // Adjust the margin as needed
             borderRadius: 'sm',
           }}
         >
           <Link to="product/">
-          <List sx={{ py: 'var(--ListDivider-gap)' }}>
-            {results.map((uuid) => (
-   
-             <Result uuid={uuid} key={uuid} />
-     
-            ))}
-          </List>
+            <List sx={{ py: 'var(--ListDivider-gap)' }}>
+              {results.map((uuid) => (
+                <Result uuid={uuid} key={uuid} />
+              ))}
+            </List>
           </Link>
         </Sheet>
-        
-      
-      <Filter/>
-      <SortButton/>
+      </AuctionListContainer>
+
+      <Filter />
     </>
-  )
+  );
 }
