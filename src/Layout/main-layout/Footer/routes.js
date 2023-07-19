297d034a3ae84762521aec3697055c96024af8d0
Your Name <patrick@Patricks-MacBook-Air.local>
2023-07-19 10:08:30 +0300

    changed from localhost footer links to online links

--- src/Layout/main-layout/Footer/routes.js
+++ src/Layout/main-layout/Footer/routes.js
@@ -24,29 +24,29 @@ const router = {
     {
       name: "company",
       items: [
-        { name: "about us", href: "http://localhost:3000/about-us" },
-        { name: "freebies", href: "http://localhost:3000/freebies" },
-        { name: "blog", href: "http://localhost:3000/blog" },
+        { name: "about us", href: "https://cars-bids.online/about-us" },
+        { name: "freebies", href: "https://cars-bids.online/freebies" },
+        { name: "blog", href: "https://cars-bids.online/blog" },
       ],
     },
     {
       name: "resources",
       items: [
-        { name: "affiliate program", href: "http://localhost:3000/affiliate-program"},
+        { name: "affiliate program", href: "https://cars-bids.online/affiliate-program"},
       ],
     },
     {
       name: "help & support",
       items: [
-        { name: "contact us", href: "http://localhost:3000/contact-us"},
-        { name: "knowledge center",href: "http://localhost:3000/knowledge-center" },
+        { name: "contact us", href: "https://cars-bids.online/contact-us"},
+        { name: "knowledge center",href: "https://cars-bids.online/knowledge-center" },
       ],
     },
     {
       name: "legal",
       items: [
-        { name: "terms & conditions", href: "http://localhost:3000/terms-conditions"},
-        { name: "privacy policy", href: "http://localhost:3000/privacy-policy" },
+        { name: "terms & conditions", href: "https://cars-bids.online/terms-conditions"},
+        { name: "privacy policy", href: "https://cars-bids.online/privacy-policy" },
       ],
     },
   ],
